8ad545e685f8243f6cbcc65307ded2b14ce7a37c
unknown <llepinay@FRW7PVP0227.OREDIS-VP.local>
2013-02-07 19:11:31 +0100

    plop

--- Tutorial1/Livraison.fsx
+++ Tutorial1/Livraison.fsx
@@ -16,22 +16,36 @@ module SuivitLivraisons =
     let c = qualifContext.History
             |> Seq.find(fun c -> c.ChangesetId = 36151)
     
-    //qualifContext.VersionControlServer.QueryMerges(null, null, "$/Front Office 5.0/1.Front/OrderPipe/Qualif", new ChangesetVersionSpec(36151), null, null, RecursionType.Full ).[0].
+    let mergeHistory = 
+        qualifContext.VersionControlServer.QueryMerges(null, null, "$/Front Office 5.0/1.Front/OrderPipe/Qualif", LatestVersionSpec.Latest, null, null, RecursionType.Full )
+        |> Seq.groupBy(fun m -> m.TargetVersion )
 
-    // Non livré encore
-    qualifContext.History
-        |> Seq.filter( fun h -> h.CreationDate >= System.DateTime.Parse("30/01/2013 11:57:00") )
-        |> Seq.map( fun h-> h.AssociatedWorkItems )
+    mergeHistory
+        |> Seq.iter(fun (target, source) -> 
+                printfn "%A" target
+                source |> Seq.iter(fun c-> printfn "    %A" c.SourceVersion)
+                )
+    //    |> Seq.sortBy(fun c -> c.TargetChangeset.CreationDate)
+    //    |> Seq.iter( fun c-> printfn "%A %A %A" (c.TargetChangeset.CreationDate.ToString()) c.TargetChangeset.ChangesetId c.SourceVersion )
 
     // Livré toujours en resolved
     let latestLivraison = 
         qualifContext.BuildServer.QueryBuilds("Front Office 5.0", "OrderPipe_R7")
-        |> Seq.find(fun b-> b.BuildNumber = "OrderPipe_R7_v1.0.0.290.01")
+        |> Seq.find(fun b-> b.BuildNumber = "OrderPipe_R7_v1.0.0.297.01")
+
+    type ChangesetWithMerges = 
+        {
+            Changeset:Changeset
+            MergedChangesets:seq<Changeset>
+        }
 
     qualifContext.History
         |> Seq.filter( fun h -> 
-            h.ChangesetId <= System.Convert.ToInt32(latestLivraison.SourceGetVersion.Replace("C","")) && 
-            h.WorkItems |> Seq.exists(fun w->w.State = "Resolved" ) )
+            if h.ChangesetId <= System.Convert.ToInt32(latestLivraison.SourceGetVersion.Replace("C","")) 
+                then 
+                    h.AssociatedWorkItems |> Seq.exists(fun w->w.State = "Resolved" ) 
+                else false
+                )
         |> Seq.map( fun h-> h.AssociatedWorkItems |> Seq.filter(fun w->w.State = "Resolved" ))
         |> Seq.concat
         |> Seq.distinctBy( fun h-> h.Id)
